2c0e40326ba0f73625a95cbdc7e49c1bfd375d51
Aditya Roy <63660541+theadityaroy@users.noreply.github.com>
2023-05-30 12:35:09 +0530

    Add files via upload

---
+++ Arrays/SearchInsertPosition.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int end = nums.length - 1;
+        while(low <= end){
+            int mid = (low + end)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(target < nums[mid]){
+                end = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
