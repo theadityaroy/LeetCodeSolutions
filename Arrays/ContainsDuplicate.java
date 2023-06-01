194208ac141de4cac9edb36d2dc8115c6fa7fdc4
Aditya Roy <adityaroy2308@hotmail.com>
2023-06-01 23:18:36 +0530

    Add files via upload

---
+++ Arrays/ContainsDuplicate.java
@@ -0,0 +1,18 @@
+// 217. Contains Duplicate : https://leetcode.com/problems/contains-duplicate/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                return true;
+            }
+            else{
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+}
+
+// Time Complexity : O(N)
