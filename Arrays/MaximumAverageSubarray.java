360f7754850ab428bafb2d93c8a7468fafd06dd0
Aditya Roy <adityaroy2308@hotmail.com>
2023-06-05 02:31:40 +0530

    Add files via upload

---
+++ Arrays/MaximumAverageSubarray.java
@@ -0,0 +1,18 @@
+// 643. Maximum Average Subarray I : https://leetcode.com/problems/maximum-average-subarray-i/description/
+
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double sum = 0;
+        for(int i = 0; i<k; i++){
+            sum+=nums[i];
+        }
+        double res = sum;
+        for(int i = k; i<nums.length; i++){
+            sum+=nums[i] - nums[i-k];
+            res = Math.max(res, sum);
+        }
+        return res/k;
+    }
+}
+
+// Time Complexity : O(N)
