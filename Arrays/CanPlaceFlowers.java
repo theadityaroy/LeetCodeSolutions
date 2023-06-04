793c0d9a695e272f56a841f659b55466a0f2a12e
Aditya Roy <adityaroy2308@hotmail.com>
2023-06-04 23:50:21 +0530

    Add files via upload

---
+++ Arrays/CanPlaceFlowers.java
@@ -0,0 +1,23 @@
+// 605. Can Place Flowers : https://leetcode.com/problems/can-place-flowers/
+
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if(n == 0){
+            return true;
+        }
+        for(int i = 0; i < flowerbed.length; i+=2){
+            if(i+1 < flowerbed.length && flowerbed[i+1] == 1){
+                i++;
+            }
+            else if(flowerbed[i] == 0){
+                n--;
+                if(n == 0){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+// Time Complexity : O(N)
