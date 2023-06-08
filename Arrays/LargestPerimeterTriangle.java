66e08eb65eb551e903b842f6c1fff96a104daf45
Aditya Roy <adityaroy2308@hotmail.com>
2023-06-09 02:14:56 +0530

    Add files via upload

---
+++ Arrays/LargestPerimeterTriangle.java
@@ -0,0 +1,18 @@
+// 976. Largest Perimeter Triangle : https://leetcode.com/problems/largest-perimeter-triangle/
+
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        if(nums.length < 3){
+            return 0;
+        }
+        Arrays.sort(nums);
+        for(int i = nums.length - 1; i > 1; i--){
+            if(nums[i] < nums[i-1] + nums[i-2]){
+                return nums[i] + nums[i-1] + nums[i-2];
+            }
+        }
+        return 0;
+    }
+}
+
+// Time Complexity : O(N LogN)
