98a6310e099d931d6ce63aefc667735229e89fc5
Aditya Roy <adityaroy2308@hotmail.com>
2023-06-11 12:59:47 +0530

    Add files via upload

---
+++ Arrays/CheckIfItIsAStraightLine.java
@@ -0,0 +1,20 @@
+// 1232. Check If It Is a Straight Line : https://leetcode.com/problems/check-if-it-is-a-straight-line/
+
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+        int x0 = coordinates[0][0];
+        int y0 = coordinates[0][1];
+        int x1 = coordinates[1][0];
+        int y1 = coordinates[1][1];
+        for(int i = 2; i < coordinates.length; i++){
+            int x = coordinates[i][0];
+            int y = coordinates[i][1];
+            if((x - x0) * (y1 - y0) != (y - y0) * (x1 - x0)){
+                return false;
+            }
+        }
+        return true;
+    } 
+}
+
+// Time Complexity : O(N)
